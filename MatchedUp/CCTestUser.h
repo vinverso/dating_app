29591adca5d6cec36b0f5eac8fc1b54e08816cae
Vincent Inverso <vinverso@vincents-mbp.home>
2014-02-26 13:45:01 -0500

    create test user

---
+++ MatchedUp/CCTestUser.h
@@ -0,0 +1,13 @@
+//
+//  CCTestUser.h
+//  MatchedUp
+//
+//  Created by Vincent Inverso on 2/26/14.
+//  Copyright (c) 2014 Vincent Inverso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTestUser : NSObject
+
+@end
---
+++ MatchedUp/CCTestUser.m
@@ -0,0 +1,13 @@
+//
+//  CCTestUser.m
+//  MatchedUp
+//
+//  Created by Vincent Inverso on 2/26/14.
+//  Copyright (c) 2014 Vincent Inverso. All rights reserved.
+//
+
+#import "CCTestUser.h"
+
+@implementation CCTestUser
+
+@end
